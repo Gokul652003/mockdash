f6fa681de982098de84668806cea6f2a1f7fe29a
Gokul KR <gokulkr652003@gmail.com>
2026-08-07 21:38:58 +0530

    feat: expose log and status event subscriptions

--- preload.js
+++ preload.js
@@ -8,4 +8,7 @@ contextBridge.exposeInMainWorld('api', {
   addEndpoint: (ep) => ipcRenderer.invoke('endpoints:add', ep),
   updateEndpoint: (ep) => ipcRenderer.invoke('endpoints:update', ep),
   removeEndpoint: (id) => ipcRenderer.invoke('endpoints:remove', id),
+
+  onLog: (cb) => ipcRenderer.on('server:log', (_e, data) => cb(data)),
+  onStatus: (cb) => ipcRenderer.on('server:status', (_e, data) => cb(data)),
 });
